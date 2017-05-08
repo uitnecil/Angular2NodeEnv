383fc49d4dd5c956dece2c785b485529ea445169
Mugur <uitnecil@gmail.com>
2017-05-08 19:36:03 +0300

    Changes: 1. Repair Env -> broken by prev changes to TypeScript config

--- client/app/app.component.spec.ts
+++ client/app/app.component.spec.ts
@@ -1,17 +1,17 @@
 /**
  * Created by zoriana.kovacs on 21/12/16.
  */
-// import { TestBed } from '@angular/core/testing';
-// import { App } from './app.component';
-// describe('App', () => {
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({ declarations: [App]});
-//     });
-//     it ('should work', () => {
-//         let fixture = TestBed.createComponent(App);
-//         expect(fixture.componentInstance instanceof App).toBe(true, 'should create AppComponent');
-//     });
-// });
+import { TestBed } from '@angular/core/testing';
+import { App } from './app.component';
+describe('App', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({ declarations: [App]});
+    });
+    it ('should work', () => {
+        let fixture = TestBed.createComponent(App);
+        expect(fixture.componentInstance instanceof App).toBe(true, 'should create AppComponent');
+    });
+});
 
 // LICE: am comentat asta pentru ca dadea erorile astea
 /*
